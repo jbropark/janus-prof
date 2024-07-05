de0d8f532f134c41909284d8a81c41a9d867ad3f
JayPark <jbropark123@gmail.com>
2024-07-05 15:34:28 +0000

    feat: Implement measure and fix streamer

---
+++ measure.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+if [ "$#" -ne 1 ]; then
+  echo "usage: $0 <dir>"
+  exit 1
+fi
+
+DIR="$1"
+
+echo "create directory '$DIR'"
+mkdir $DIR || exit 1
+
+for i in {1..5}
+do
+docker stats --format json --no-stream > "$DIR/dstat-$i.txt"
+sleep 1
+vnstat -tr 5 -i janus --json > "$DIR/vnstat-$i.txt"
+sleep 1
+vmstat 1 10 > "$DIR/vmstat-$i.txt"
+sleep 1
+done
+
+echo "success to save measures to '$DIR'"
+
+cat $DIR/*-1.txt
--- streamer.sh
+++ streamer.sh
@@ -3,17 +3,19 @@
 # Default values
 HOST="172.20.0.2"
 PORT=5002
-SIZE=1
+FRAMERATE=30
+BITRATE=1000000
 MTU=1500
 
 # Function to display usage information
 usage() {
   echo "Usage: $0 [--host host] [--port port] [--size size] [--mtu mtu]" 1>&2
   echo "Options:" 1>&2
-  echo "  --host host    Hostname or IP address (default: $HOST)" 1>&2
-  echo "  --port port    Port number (default: $PORT)" 1>&2
-  echo "  --size size    Size of image (default: $SIZE)" 1>&2
-  echo "  --mtu  mtu     MTU (default: $MTU)" 1>&2
+  echo "  --host host          Hostname or IP address (default: $HOST)" 1>&2
+  echo "  --port port          Port number (default: $PORT)" 1>&2
+  echo "  --frame framerate    Framerate of testsrc (default: $FRAMERATE)" 1>&2
+  echo "  --bit bitrate        Bitrate of video encoding (default: $BITRATE)" 1>&2
+  echo "  --mtu  mtu           MTU (default: $MTU)" 1>&2
   exit 1
 }
 
@@ -28,8 +30,12 @@ while [ $# -gt 0 ]; do
       PORT=$2
       shift 2
       ;;
-    --size)
-      SIZE=$2
+    --bit)
+      BITRATE=$2
+      shift 2
+      ;;
+    --frame)
+      FRAMERATE=$2
       shift 2
       ;;
     --mtu)
@@ -47,19 +53,14 @@ while [ $# -gt 0 ]; do
   esac
 done
 
-WIDTH=$((320 * $SIZE))
-HEIGHT=$((240 * $SIZE))
-BITRATE=$((1000000 * $SIZE * $SIZE))
 APORT=$PORT
 VPORT=$((PORT + 2))
 MTUARG=$((MTU - 100))
 echo "Host: $HOST"
 echo "Audio Port: $APORT"
 echo "Video Port: $VPORT"
-echo "Size: $SIZE"
-echo "Width: $WIDTH"
-echo "Height: $HEIGHT"
-echo "BR: $BITRATE"
+echo "BitRate: $BITRATE"
+echo "FrameRate: $FRAMERATE"
 echo "MTUARG: $MTUARG"
 
 gst-launch-1.0 \
@@ -70,10 +71,10 @@ gst-launch-1.0 \
                 rtpopuspay ! \
 		udpsink host=$HOST port=$APORT \
         videotestsrc ! \
-		video/x-raw,width=$WIDTH,height=$HEIGHT ! \
-		queue ! videoscale ! \
-		queue ! videorate ! \
-		queue ! videoconvert ! \
-		queue ! timeoverlay ! \
-		queue ! vp8enc target-bitrate=$BITRATE error-resilient=1 ! \
+		video/x-raw,width=400,height=300,framerate=$FRAMERATE/1 ! \
+		videoscale ! \
+		videorate ! \
+		videoconvert ! \
+		timeoverlay ! \
+		vp8enc target-bitrate=$BITRATE error-resilient=1 ! \
 		rtpvp8pay mtu=$MTUARG ! udpsink host=$HOST port=$VPORT
